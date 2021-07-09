ac0f37b296ec93f9370b981489b1a17c455e654c
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-07-09 13:40:27 +0200

    singleMovie component

---
+++ src/components/SingleMovie.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal } from "react-bootstrap";
+import { useState } from "react";
+
+const SingleMovie = (props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <img
+        src={props.img}
+        className="thumbnail"
+        alt="thumbnail"
+        
+        onClick={handleShow}
+      />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <CommentList /* COMMENTS */ />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
