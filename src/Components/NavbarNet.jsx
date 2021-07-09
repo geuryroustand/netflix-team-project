67ac3f8606e1152bda8bed5908771a72acc8f5f7
ako652 <akotabe0213@gmail.com>
2021-07-09 16:21:23 +0200

    combine

--- src/Components/NavbarNet.jsx
+++ src/Components/NavbarNet.jsx
@@ -1,17 +1,21 @@
+import { right } from "@popperjs/core";
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container,Dropdown } from "react-bootstrap";
+import {Search,BellFill} from  "react-bootstrap-icons"
+
 
 const NavbarNet = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <div className="position-relative mycolor">
+    <Navbar bg="dark" expand="lg">
       <Navbar>
         <Navbar.Brand href="#home">
           <img
             src="./netflix.png"
-            width="40"
-            height="40"
+            width="100"
+            height="height:48px;"
             className="d-inline-block align-top"
-            alt="React Bootstrap logo"
+            alt=""
           />
         </Navbar.Brand>
       </Navbar>
@@ -20,7 +24,10 @@ const NavbarNet = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#link">TV shows</Nav.Link>
+          <Nav.Link href="#link">movies</Nav.Link>
+          <Nav.Link href="#link">recently added</Nav.Link>
+          <Nav.Link href="#link">my list</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
@@ -35,7 +42,54 @@ const NavbarNet = () => {
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  );
+
+<div className="d-flex justify-content m-1" >
+  <h3 className="pl-3">TV SHOWS</h3>
+    <Dropdown className="pl-3">
+  <Dropdown.Toggle variant="dark" id="dropdown-basic">
+    Genres
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</div>
+<div style={{color: "rgb(117, 117, 117)"}} size= {30} className="position-absolute mystyle d-flex justify-content-between"
+>
+  <div className="p-3">
+<Search />
+</div>
+<div className="p-3">
+<BellFill  />
+</div>
+<span className="m-3">KIDS</span>
+<span>
+<Dropdown className="m-1">
+  <Dropdown.Toggle variant="dark" id="dropdown-basic">
+    Genres
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</span>
+
+
+</div>
+
+
+    </div>
+
+    
+  )
+
+
 };
 
 export default NavbarNet;
